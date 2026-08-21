a6795db0823473a844a62ca14c13dcbb9285ae6f
Justin Too <justintoo224@gmail.com>
2026-08-21 13:45:35 +0800

    added personality to greet and exit

--- src/main/java/PeanutButterCat.java
+++ src/main/java/PeanutButterCat.java
@@ -3,9 +3,17 @@
  */
 public class PeanutButterCat {
     public static void main(String[] args) {
+        String horizontalLine = "____________________________________________________________";
         String banner = " /\\_/\\\n"
                 + "( o.o )  peanutbuttercat\n"
                 + " > u <";
+
+        System.out.println(horizontalLine);
         System.out.println(banner);
+        System.out.println("Hey! I'm peanutbuttercat, and I'm pawsitively ready to help!");
+        System.out.println("What awesome task can we tackle together?");
+        System.out.println(horizontalLine);
+        System.out.println("You're amazing! Keep it up, and I'll see you again soon!");
+        System.out.println(horizontalLine);
     }
 }
