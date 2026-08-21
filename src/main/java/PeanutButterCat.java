b59767a1541778d657e88834298677c6bde06e2b
Justin Too <justintoo224@gmail.com>
2026-08-21 14:21:50 +0800

    unmark without classes

--- src/main/java/PeanutButterCat.java
+++ src/main/java/PeanutButterCat.java
@@ -10,6 +10,7 @@ public class PeanutButterCat {
                 + "( o.o )  peanutbuttercat\n"
                 + " > u <";
         String[] tasks = new String[100];
+        boolean[] isDone = new boolean[100];
         int taskCount = 0;
 
         System.out.println(horizontalLine);
@@ -31,9 +32,21 @@ public class PeanutButterCat {
             }
 
             if (command.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println((i + 1) + ". " + tasks[i]);
+                    String statusIcon = isDone[i] ? "[X]" : "[ ]";
+                    System.out.println((i + 1) + "." + statusIcon + " " + tasks[i]);
                 }
+            } else if (command.startsWith("mark ")) {
+                int taskIndex = Integer.parseInt(command.substring(5)) - 1;
+                isDone[taskIndex] = true;
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("  [X] " + tasks[taskIndex]);
+            } else if (command.startsWith("unmark ")) {
+                int taskIndex = Integer.parseInt(command.substring(7)) - 1;
+                isDone[taskIndex] = false;
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("  [ ] " + tasks[taskIndex]);
             } else {
                 tasks[taskCount] = command;
                 taskCount++;
